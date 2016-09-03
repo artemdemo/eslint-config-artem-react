172ba852f5987829d3bf5f8dc647fbb5616736a0
Artem Demo <artemdemo@gmail.com>
2016-09-03 13:15:40 +0300

    added rules

---
+++ index.js
@@ -0,0 +1,18 @@
+module.exports = {
+	"extendes": [
+        "airbnb",
+        "artem"
+    ],
+	"rules": {
+		"react/prop-types": 0,
+        "react/jsx-indent": ["error", 4],
+        "react/jsx-indent-props": 0,
+        "react/jsx-first-prop-new-line": 0,
+        "react/jsx-closing-bracket-location": 0,
+        "react/prefer-stateless-function": 0,
+        "react/no-did-mount-set-state": 0,
+        "react/jsx-pascal-case": 0,
+        "react/jsx-no-bind": 0,
+        "jsx-quotes": ["error", "prefer-single"]
+	}
+};
